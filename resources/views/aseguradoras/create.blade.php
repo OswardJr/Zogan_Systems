197e1e12271a9f45abace0cdf11a1261634fb58d
aranajhonny <jhonnyjosearana@gmail.com>
2017-06-26 18:47:49 -0430

    modificacion de alertas. #4

--- resources/views/aseguradoras/create.blade.php
+++ resources/views/aseguradoras/create.blade.php
@@ -1,71 +1,71 @@
 @include('layouts.headservices')
 
-                          <div class="col-md-12">
-                              @if(Session::get('message'))
-                              <div class="col-md-6 col-md-offset-6">
-                                  <div class="alert alert-danger alert-dismissable">
-                                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                                    <h3> {{ Session::get('message') }}</h3>
-                                  </div>
-                              </div>
-                              @endif                            
-                          </div> 
-                <div class="page-content-wrapper" style="">
-                    <div class="page-content">                    
-                        <section class="content">
-                          <div class="row">
-                            <div class="col-md-12 ">
-                                  <h3>Nueva Aseguradora</h3>
-                                <hr>
+<div class="col-md-12">
+  @if(Session::get('message'))
+  <div class="col-md-10 col-md-offset-2">
+    <div class="alert alert-success alert-dismissable">
+      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+      <h3> {{ Session::get('message') }}</h3>
+    </div>
+  </div>
+  @endif
+</div>
+<div class="page-content-wrapper" style="">
+  <div class="page-content">
+    <section class="content">
+      <div class="row">
+        <div class="col-md-12 ">
+          <h3>Nueva Aseguradora</h3>
+          <hr>
 
-                                <div class="panel panel-primary">
-                                <div class="panel-heading ">
-                                  <center>
-                                    <h4>Datos de la Aseguradora</h4>
-                                  </center>
-                                </div>
+          <div class="panel panel-primary">
+            <div class="panel-heading ">
+              <center>
+                <h4>Datos de la Aseguradora</h4>
+              </center>
+            </div>
 
-                                <div class="panel-body ">
-                                  <form method="post" action="{{ url('/aseguradoras') }}">
-                                   <input required="true" type="hidden" name="_token" value="{{ csrf_token() }}">
-                                    <div class="form-group col-xs-6 " style="margin-bottom: 0px; height: 60px">
-                                      <label>Rif<a class="campos-required" title="Campo Obligatorio."> *</a></label>
-                                      <div class="input-group input-group-sm">
-                                        <input type="text" name="rif" id="" class="form-control " pattern="^([JVEG]{1})-([0-9]{8})-([0-9]{1})$" title="El formato debe ser J-12345678-9"  placeholder="J-12345678-9" onkeyup="this.value=this.value.toUpperCase()" value="" required="true" >
-                                        <span class="input-group-btn">
-                                          <button  data-toggle="tooltip" title="Consultar" class="btn btn-buscar btn-flat fa fa-search
-                                          " type="button" name="btn-search"></button>
-                                        </span>
-                                      </div>
-                                      <center><span id="mensaje" class="help-block"></span></center>
-                                    </div>
-                                    <div class="form-group col-xs-6">
-                                      <label>Denominación<a class="campos-required" pattern="[A-Z]" title="Campo Obligatorio."> *</a></label>
-                                      <input type="text" name="denominacion" id="" class="form-control" placeholder="Seguros Caracas C.A" required="true">
-                                    </div>
-                                    <div class="form-group col-xs-6">
-                                      <label>Teléfono<a class="campos-required" title="Campo Obligatorio."> *</a></label>
-                                      <input type="text" name="telefono" id="" pattern="^([0-9]{4})-([0-9]{7})$" class="form-control" placeholder="0212-XXXXXXX">
-                                    </div>
-                                    <div class="form-group col-xs-6">
-                                      <label>Email<a class="campos-required" title="Campo Obligatorio."> *</a></label>
-                                      <input type="email" name="email" id="email" class="form-control" placeholder="contact@example.com">
-                                    </div>
-                                    <center class="col-xs-offset-3 col-xs-6">
-                                      <span class="" style="font-weight:bold;">Los campos marcados con <a class="obli" rel="tooltip" style="font-size:20px;">*</a> son Obligatorios.</span><br><br>
-                                      <button data-toggle="tooltip" title="Guardar" type="submit" class="btn btn-guardar margin glyphicon glyphicon-floppy-disk" name="agregar"></button>
-                                      <button data-toggle="tooltip" title="Limpiar Formulario" type="reset" class="btn btn-refresh margin glyphicon glyphicon-repeat"></button>
-                                    </center>
-                                  </form>
-                              </div>
-
-                              </div><!-- /.modal-content -->
-                            </div><!-- /.modal-dialog -->
-                          </div>
-                        </div><!-- /.col -->
-                        </div><!-- /.row -->
-                        </section><!-- /.content -->
-                    </div>
+            <div class="panel-body ">
+              <form method="post" action="{{ url('/aseguradoras') }}">
+               <input required="true" type="hidden" name="_token" value="{{ csrf_token() }}">
+               <div class="form-group col-xs-6 " style="margin-bottom: 0px; height: 60px">
+                <label>Rif<a class="campos-required" title="Campo Obligatorio."> *</a></label>
+                <div class="input-group input-group-sm">
+                  <input type="text" name="rif" id="" class="form-control " pattern="^([JVEG]{1})-([0-9]{8})-([0-9]{1})$" title="El formato debe ser J-12345678-9"  placeholder="J-12345678-9" onkeyup="this.value=this.value.toUpperCase()" value="" required="true" >
+                  <span class="input-group-btn">
+                    <button  data-toggle="tooltip" title="Consultar" class="btn btn-buscar btn-flat fa fa-search
+                    " type="button" name="btn-search"></button>
+                  </span>
                 </div>
+                <center><span id="mensaje" class="help-block"></span></center>
+              </div>
+              <div class="form-group col-xs-6">
+                <label>Denominación<a class="campos-required" pattern="[A-Z]" title="Campo Obligatorio."> *</a></label>
+                <input type="text" name="denominacion" id="" class="form-control" placeholder="Seguros Caracas C.A" required="true">
+              </div>
+              <div class="form-group col-xs-6">
+                <label>Teléfono<a class="campos-required" title="Campo Obligatorio."> *</a></label>
+                <input type="text" name="telefono" id="" pattern="^([0-9]{4})-([0-9]{7})$" class="form-control" placeholder="0212-XXXXXXX">
+              </div>
+              <div class="form-group col-xs-6">
+                <label>Email<a class="campos-required" title="Campo Obligatorio."> *</a></label>
+                <input type="email" name="email" id="email" class="form-control" placeholder="contact@example.com">
+              </div>
+              <center class="col-xs-offset-3 col-xs-6">
+                <span class="" style="font-weight:bold;">Los campos marcados con <a class="obli" rel="tooltip" style="font-size:20px;">*</a> son Obligatorios.</span><br><br>
+                <button data-toggle="tooltip" title="Guardar" type="submit" class="btn btn-guardar margin glyphicon glyphicon-floppy-disk" name="agregar"></button>
+                <button data-toggle="tooltip" title="Limpiar Formulario" type="reset" class="btn btn-refresh margin glyphicon glyphicon-repeat"></button>
+              </center>
+            </form>
+          </div>
 
-@include('layouts.footer')   
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div>
+  </div><!-- /.col -->
+</div><!-- /.row -->
+</section><!-- /.content -->
+</div>
+</div>
+
+@include('layouts.footer')
--- resources/views/listasegu.blade.php
+++ resources/views/listasegu.blade.php
@@ -1,70 +1,70 @@
 @include('layouts.headservices')
 
-                          <br><div class="col-md-12">
-                              @if(Session::get('message'))
-                              <div class="col-md-6 col-md-offset-6">
-                                  <div class="alert alert-danger alert-dismissable">
-                                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                                    <h3> {{ Session::get('message') }}</h3>
-                                  </div>
-                              </div>
-                              @endif                            
-                          </div>  
-                <div class="page-content-wrapper" style="">
-                    <div class="page-content">                    
-                        <section class="content">
-                            <h3>Listado de Aseguradoras</h3>
-                                <hr>                         
-                            <div class="panel panel-primary">
-                                <div class="panel-heading"><strong>Aseguradoras</strong></div>
-                          <div class="row">
-                            <div class="col-md-12 ">
-                            <div class="table-responsive">
-                                <table data-toggle="table" class="table table-bordered table-striped">
-                                <thead>
-                                <tr>
-                                    <th>Cédula/Rif</th>
-                                    <th>Denominación</th>
-                                    <th>Teléfono</th>
-                                    <th>Email</th>
-                                    <th>Acciones</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($aseguradoras as $asegu)
-                                        <tr> 
-                                          <td>{{ ($asegu->rif) }}</td> 
-                                          <td>{{ ($asegu->denominacion) }}</td> 
-                                          <td>{{ ($asegu->telefono) }}</td> 
-                                          <td>{{ ($asegu->email) }}</td>             
-                                    <td>
-                                        <a href="{{ route('aseguradoras.show', $asegu->id) }}" class="btn btn-buscar" data-toggle="tooltip" title="Ver"><i class="fa fa-eye"></i>
-                                            
-                                        </a>
+<br><div class="col-md-12">
+@if(Session::get('message'))
+<div class="col-md-10 col-md-offset-2">
+  <div class="alert alert-success alert-dismissable">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    <h3> {{ Session::get('message') }}</h3>
+  </div>
+</div>
+@endif
+</div>
+<div class="page-content-wrapper" style="">
+  <div class="page-content">
+    <section class="content">
+      <h3>Listado de Aseguradoras</h3>
+      <hr>
+      <div class="panel panel-primary">
+        <div class="panel-heading"><strong>Aseguradoras</strong></div>
+        <div class="row">
+          <div class="col-md-12 ">
+            <div class="table-responsive">
+              <table data-toggle="table" class="table table-bordered table-striped">
+                <thead>
+                  <tr>
+                    <th>Cédula/Rif</th>
+                    <th>Denominación</th>
+                    <th>Teléfono</th>
+                    <th>Email</th>
+                    <th>Acciones</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach ($aseguradoras as $asegu)
+                  <tr>
+                    <td>{{ ($asegu->rif) }}</td>
+                    <td>{{ ($asegu->denominacion) }}</td>
+                    <td>{{ ($asegu->telefono) }}</td>
+                    <td>{{ ($asegu->email) }}</td>
+                    <td>
+                      <a href="{{ route('aseguradoras.show', $asegu->id) }}" class="btn btn-buscar" data-toggle="tooltip" title="Ver"><i class="fa fa-eye"></i>
 
-                                        <a href="{{ route('aseguradoras.edit', $asegu->id) }}" class="btn btn-editar" data-toggle="tooltip" title="Actualizar"><i class="fa fa-pencil"></i>
-                                            
-                                        </a>
+                      </a>
 
-                                        <a class="btn btn-delete" data-toggle="tooltip" title="Eliminar"><i class="fa fa-trash"></i>
-                                            
-                                        </a>
-                                    </td>
-                                        </tr>
-                                        @endforeach
+                      <a href="{{ route('aseguradoras.edit', $asegu->id) }}" class="btn btn-editar" data-toggle="tooltip" title="Actualizar"><i class="fa fa-pencil"></i>
 
-                                </tbody>
-                            </table>
-                         </div>      
-                                                                 {{ $aseguradoras->links() }}
-                        
-                          
-                                </div><!-- /.col -->
-                            </div><!-- /.row -->
-                        </div>
-                    </div>                 
-                        </section><!-- /.content -->
-                    </div>
-                </div>
+                      </a>
 
-@include('layouts.footer')        
+                      <a class="btn btn-delete" data-toggle="tooltip" title="Eliminar"><i class="fa fa-trash"></i>
+
+                      </a>
+                    </td>
+                  </tr>
+                  @endforeach
+
+                </tbody>
+              </table>
+            </div>
+            {{ $aseguradoras->links() }}
+
+
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div>
+    </div>
+  </section><!-- /.content -->
+</div>
+</div>
+
+@include('layouts.footer')
