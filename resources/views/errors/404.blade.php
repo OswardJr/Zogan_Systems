425b610d35c18c6fac747400634282ab944b195e
aranajhonny <jhonnyjosearana@gmail.com>
2017-07-12 00:40:36 -0430

    add custom 404.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+<!-- Main content -->
+@section('content')
+<section class="content">
+  <div class="row">
+      <div class="callout callout-danger" style="margin-bottom: 0!important;">
+        <center><h2><i class="fa fa-ban"></i> Error 404</h2></center>
+      </div>
+  </div>
+</section>
+@endsection
